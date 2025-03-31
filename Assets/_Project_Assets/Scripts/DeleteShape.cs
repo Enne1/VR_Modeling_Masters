840151044f415fb13f4ed74f3048855225e9d2ee
Enne1 <60690875+Enne1@users.noreply.github.com>
2025-03-31 15:47:57 +0200

    Added Wireframe and Delete indicator. Cleaned up

--- Assets/_Project_Assets/Scripts/DeleteShape.cs
+++ Assets/_Project_Assets/Scripts/DeleteShape.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
 
 public class DeleteShape : MonoBehaviour
 {
     private float spawnRadius = 0.2f; 
     public LayerMask detectionLayer; 
+    private Material originalMat;
+    public Material deleteMat;
+    
+    //private bool isObjectInside = false;  // A boolean to keep track of whether an object is inside the collider
     
     // Code Run whenever an object it released from being grabbed
     public void Deletion()
@@ -16,5 +21,28 @@ public class DeleteShape : MonoBehaviour
         {
             Destroy(obj.gameObject);
         }
-    } 
+    }
+
+    // Called when another collider enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered the trigger is on the specified layer mask
+        if ((detectionLayer.value & (1 << other.gameObject.layer)) != 0)
+        {
+            originalMat = other.gameObject.GetComponent<MeshRenderer>().material;
+            //isObjectInside = true;  // Set the boolean to true when an object enters
+            other.gameObject.GetComponent<MeshRenderer>().material = deleteMat;
+        }
+    }
+
+    // Called when another collider exits the trigger collider
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the object that exited the trigger is on the specified layer mask
+        if ((detectionLayer.value & (1 << other.gameObject.layer)) != 0)
+        {
+            //isObjectInside = false;  // Set the boolean to false when an object leaves
+            other.gameObject.GetComponent<MeshRenderer>().material = originalMat;
+        }
+    }
 }
---
+++ Assets/_Project_Assets/Scripts/Visualizers/WireframeRenderer.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ProBuilderMesh))]
+public class ProBuilderWireframe : MonoBehaviour
+{
+    public Color wireColor = Color.green; // Wireframe color
+    private ProBuilderMesh pbMesh;
+    private HashSet<Edge> quadEdges = new HashSet<Edge>();
+    private int lastVertexCount = 0; // Track mesh changes
+
+    private void Start()
+    {
+        pbMesh = GetComponent<ProBuilderMesh>();
+        UpdateWireframe(); // Initial setup
+    }
+
+    private void Update()
+    {
+        // Check if the mesh has changed
+        if (pbMesh.vertexCount != lastVertexCount)
+        {
+            UpdateWireframe();
+            lastVertexCount = pbMesh.vertexCount;
+        }
+    }
+
+    private void OnRenderObject()
+    {
+        if (quadEdges.Count == 0) return;
+
+        // Use GL to render the edges
+        Material lineMaterial = new Material(Shader.Find("Hidden/Internal-Colored"));
+        lineMaterial.SetPass(0);
+        GL.Begin(GL.LINES);
+        GL.Color(wireColor);
+
+        foreach (Edge edge in quadEdges)
+        {
+            DrawEdge(pbMesh.positions[edge.a], pbMesh.positions[edge.b]);
+        }
+
+        GL.End();
+    }
+
+    private void UpdateWireframe()
+    {
+        quadEdges.Clear(); // Clear previous edges
+        Dictionary<Edge, int> edgeCount = new Dictionary<Edge, int>();
+
+        foreach (Face face in pbMesh.faces)
+        {
+            if (face.edges.Count == 4) // Ensure the face is a quad
+            {
+                foreach (Edge edge in face.edges)
+                {
+                    edgeCount.TryGetValue(edge, out int count);
+                    edgeCount[edge] = count + 1;
+                }
+            }
+        }
+
+        // Only add edges that are part of quads
+        foreach (var kvp in edgeCount)
+        {
+            if (kvp.Value == 1) // Unique edges (not internal)
+                quadEdges.Add(kvp.Key);
+        }
+
+        pbMesh.ToMesh();
+        pbMesh.Refresh();
+    }
+
+    private void DrawEdge(Vector3 v0, Vector3 v1)
+    {
+        GL.Vertex(transform.TransformPoint(v0));
+        GL.Vertex(transform.TransformPoint(v1));
+    }
+}
